7041f2fcd44c6d74217ce90c1fcc7afa91f3717a
michaelwitting <michael.witting@helmholtz-muenchen.de>
2021-06-22 10:38:12 +0200

    update indexing function

--- R/retentionIndexing.R
+++ R/retentionIndexing.R
@@ -2,13 +2,13 @@
 #'
 #' @description
 #'
-#' `rtimeIndexing` uses a list of known substances to convert retention times to
+#' `indexRtime` uses a list of known substances to convert retention times to
 #'     retention indices. By default linear interpolation is performed.
 #'
 #' @param x `numeric` vector with retention times
 #' 
 #' @param y `data.frame`data.frame containing two columns, where the first 
-#'     holds the retention times of the indexing substancs and the second the
+#'     holds the retention times of the indexing substances and the second the
 #'    actual index value
 #' 
 #' @param FUN `function` function defining how the conversion is performed, 
@@ -16,7 +16,9 @@
 #'     
 #' @param ... additional parameter used by `FUN`
 #'
-#' @return `numeric` vector of same length as x with retention indices
+#' @return `numeric` vector of same length as x with retention indices. Values 
+#'     floating point decimals. If integer values shall be used conversion has
+#'     to be performed manually
 #'
 #' @author Michael Witting
 #'
@@ -27,12 +29,21 @@
 #' rti <- data.frame(rtime = c(1,2,3),
 #' rindex = c(100,200,300))
 #' rtime <- c(1.5, 2.5)
-#' rtimeIndexing(rtime, rti)
+#' indexRtime(rtime, rti)
 #' 
-rtimeIndexing <- function(x,
-                          y,
-                          FUN = rtiLinear,
-                          ...) {
+indexRtime <- function(x,
+                       y,
+                       FUN = rtiLinear,
+                       ...) {
+  
+  # sanity checks
+  if(missing(y)) {
+    stop("Missing data.frame with index data")
+  }
+  
+  if(!all(c("rtime", "rindex") %in% colnames(y))) {
+    stop("Missing column 'rtime', 'rindex' or both")
+  }
   
   FUN <- match.fun(FUN)
   do.call(FUN, list(x = x,
@@ -43,12 +54,13 @@ rtimeIndexing <- function(x,
 
 #' function for linear interpolation
 #' @noRd
-rtiLinear <- function(x, rti) {
+rtiLinear <- function(x, rti, yleft = NA, yright = NA, ...) {
   
-  approx(rti[,1],
-         rti[,2],
+  approx(rti$rtime,
+         rti$rindex,
          x,
-         yleft = NA,
-         yright = NA)$y
+         yleft = yleft,
+         yright = yright,
+         ...)$y
   
 }
--- tests/testthat/test_retentionIndexing.R
+++ tests/testthat/test_retentionIndexing.R
@@ -1,17 +1,26 @@
 test_that("retention indexing linear works", {
   
-  rti <- read.table(system.file("retentionIndex/rti.txt",
+  # load test data
+  rti <- read.table(system.file("retentionIndex",
+                                "rti.txt",
                                 package = "MetaboCoreUtils"),
                     header = TRUE,
                     sep = "\t")
   
-  rtime <- read.table(system.file("retentionIndex/metabolites.txt",
+  rtime <- read.table(system.file("retentionIndex",
+                                  "metabolites.txt",
                                   package = "MetaboCoreUtils"),
                       header = TRUE,
                       sep = "\t")
   
-  rtime$rindex2 <- rtimeIndexing(rtime$rtime, rti)
+  rtime$rindex_linear <- indexRtime(rtime$rtime, rti)
   
-  expect_equal(round(rtime$rindex_manual, 0), round(rtime$rindex2, 0))
+  expect_equal(round(rtime$rindex_manual, 0), round(rtime$rindex_linear, 0))
+  
+  rti <- data.frame(rt = c(1,2,3),
+                    rti = c(100,200,300))
+  
+  expect_error(indexRtime(rtime$rtime, rti), "Missing column 'rtime', 'rindex' or both")
+  expect_error(indexRtime(rtime$rime), "Missing data.frame with index data")
 
 })
