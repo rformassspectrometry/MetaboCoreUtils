94f64c3b0852193e0f1c099ca24dac730ee8ca59
jorainer <johannes.rainer@gmail.com>
2022-01-25 12:42:32 +0100

    tests: disable unit test for onLoad

--- tests/testthat/test_zzz.R
+++ tests/testthat/test_zzz.R
@@ -1,5 +1,5 @@
 test_that(".onLoad works", {
-    MetaboCoreUtils:::.onLoad(pkgname = "MetaboCoreUtils")
+    ## MetaboCoreUtils:::.onLoad(pkgname = "MetaboCoreUtils")
     ## Check that we have .ADDUCTS_ADD
     expect_true(length(MetaboCoreUtils:::.ADDUCTS_ADD) > 0)
 })
