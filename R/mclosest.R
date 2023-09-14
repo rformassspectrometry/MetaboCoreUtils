0fa498c052d92cf57eba13672b55ccbc2850a237
Philippine Louail <127301965+philouail@users.noreply.github.com>
2023-09-14 18:12:31 +0200

    Update mclosest.R

--- R/mclosest.R
+++ R/mclosest.R
@@ -6,65 +6,59 @@
 #'
 #' `mclosest` ...
 #'
-#' @param x `numeric` vector with migration times in minutes.
+#' @param 
 #'
-#' @param rtime `numeric` vector that holds the migration times (in minutes) of
-#' either one or two EOF markers in the same run of which the migration time is
-#' going to be transformed.
+#' @param 
 #'
-#' @return `numeric` vector of same length as x with effective mobility values.
+#' @return 
 #'
 #' @author Philippine Louail
 #'
 #' @export
 #'
 #' @examples
-
-#' 
-#' ## short code test
-#'
-x <- data.frame(a= 1:5, b= 3, colnames([a:e]))
-table <- data.frame(c= 2,d=4:8)
+x <- data.frame(a= 1:5, b= 3)
+table <- data.frame(c= 2:6,d=4:8)
 ppm <- 0.5
-#' 
-#' ## take x row and get absulotue difference with every table row and get another table  
-#' look for the outer 
-#' take the first row of X - duplicate it s that it the lenght of the table argument then apply outer(X,Y, FUN=*-* or diff
-#' maybe no need to replicate, i think it is within the function alreadz 
-x1 <- x[1,]
-x <- as.array(x)
-outer(X = as.array(x),Y = as.array(table))
 
-#' 
-#'## looping through different rows of X  
-#' 
-#' ## rank / order the column of an array 
-#' 
-#' 
-#' ## multiple these rank per row 
-#'look for %*% (inner matrix vector multiplication)
-#' 
-#' get index for the minimum
-#' 
-#' 
-#' 
+# ## little benchmark on rep vs replicate vs apply = rep
+# microbenchmark(
+#   abs(as.matrix(table) - matrix(do.call(rbind, rep(x1, nrow(table))),
+#          ncol = ncol(table), byrow =TRUE)),
+#   abs(as.matrix(table) - as.matrix(do.call(rbind, 
+#                                            replicate(n = nrow(table), x1, 
+#                                                      simplify = FALSE)))),
+#   t(apply(table, MARGIN = 1, function(z) z - x2)) # x2 <- as.numeric(x1)
+
+
 mclosest <- function(x, 
                      table, 
                      ppm = 0, 
                      tolerance = Inf) {
   
   # sanity checks
-  if (dimnames(x) == NULL) # no clue why 
-    stop("'x' need to be an array")
-  if (dimnames(table) == NULL) # no clue why 
-    stop("'table' need to be an array")
-  if (ncol(x) != ncol(table)) #works
+  if (!is.array(x))  # I also tried dimnames == NULL but my table do not work here either. 
+    stop("'x' needs to be an array")
+  if (!is.array(table))
+    stop("'table' needs to be an array")
+  if (ncol(x) != ncol(table))
     stop(" 'x' and 'table' need ot have same number of columns")
   if (length(ppm) != ncol(x))
     ppm <- seq(ppm, by = 0 , along.with = x[1,]) #works but maybe better way
   
-  
-  
+  for (i in 1:nrow(x)) { 
+    x1 <- x[i,, drop=FALSE]
+    x1<- matrix(do.call
+                (rbind, rep(x1, nrow(table))), 
+                ncol = ncol(table), byrow =TRUE)
+    abdiff <- abs(as.matrix(table) - x1)
+    ranked <- apply(abdiff, ncol(abdiff), rank)
+    rowProd <- apply(ranked, 1, prod)
+    return(which.min(rowProd))
+  }
   
   ...
 }
+
+#' Used apply twice, but for now cannot find anything better using basic function 
+#' 
