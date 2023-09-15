f0ff64189d0cbe04d10fdcae2b4959821e9b90ed
Philippine Louail <127301965+philouail@users.noreply.github.com>
2023-09-15 10:03:09 +0200

    Update mclosest.R

--- R/mclosest.R
+++ R/mclosest.R
@@ -17,51 +17,39 @@
 #' @export
 #'
 #' @examples
-x <- data.frame(a= 1:5, b= 3)
-table <- data.frame(c= 2:6,d=4:8)
-ppm <- 0.5
-
-# ## little benchmark on rep vs replicate vs apply = rep
-# microbenchmark(
-#   abs(as.matrix(table) - matrix(do.call(rbind, rep(x1, nrow(table))),
-#          ncol = ncol(table), byrow =TRUE)),
-#   abs(as.matrix(table) - as.matrix(do.call(rbind, 
-#                                            replicate(n = nrow(table), x1, 
-#                                                      simplify = FALSE)))),
-#   t(apply(table, MARGIN = 1, function(z) z - x2)) # x2 <- as.numeric(x1)
+#' x <- data.frame(a = 1:5, b = 3:7)
+#' table <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45))
+#' ppm <- 0.5
+#' mclosest(x, table, ppm = 0.5)
 
 
 mclosest <- function(x, 
                      table, 
                      ppm = 0, 
                      tolerance = Inf) {
-  
   # sanity checks
-  if (!is.array(x))  
-    # I also tried dimnames == NULL but my table do not work (get logical(0))
-    # here either. but do we want to make it work for table?
-    # i am a bit confused by that, is the ideal to make is work for everything ? 
+  if (is.null(dim(x)))  
     stop("'x' needs to be an array")
-  if (!is.array(table))
+  if (is.null(dim(table)))
     stop("'table' needs to be an array")
   if (ncol(x) != ncol(table))
     stop(" 'x' and 'table' need ot have same number of columns")
+  if (!is.matrix(x)) 
+    x <- as.matrix(x)
+  if (!is.matrix(table)) 
+    table <- as.matrix(table)
   if (length(ppm) != ncol(x))
-    ppm <- seq(ppm, by = 0 , along.with = x[1,]) #works but maybe better way
+   ppm <- rep(ppm[1], ncol(x))
+  
   
   for (i in 1:nrow(x)) { 
     x1 <- x[i,, drop=FALSE]
-    x1<- matrix(do.call
-                (rbind, rep(x1, nrow(table))), 
-                ncol = ncol(table), byrow =TRUE)
-    abdiff <- abs(as.matrix(table) - x1)
-    ranked <- apply(abdiff, ncol(abdiff), rank)
+    x1 <- matrix(rbind(rep(x1, nrow(table))), ncol = ncol(table), byrow = TRUE)
+    abdiff <- abs(table - x1)
+    ranked <- apply(abdiff, 2, rank)
     rowProd <- apply(ranked, 1, prod)
     return(which.min(rowProd))
   }
-  
   ...
 }
 
-#' Used apply twice, but for now cannot find anything better using basic function 
-#' 
