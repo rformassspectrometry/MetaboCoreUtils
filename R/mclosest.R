1788f7e529d4281f948b775cc866d4fb7b5c43bf
Philippine Louail <philippine.louail@outlook.com>
2023-09-29 15:55:16 +0200

    fix: description and examples

--- R/mclosest.R
+++ R/mclosest.R
@@ -2,33 +2,39 @@
 #'
 #' @description
 #'
-#' `mclosest` functions calculate the closest rows in a pairwise manner.
-#' It returns the index of the closest row in 'table' for each row in 'x'. 
+#' The `mclosest` function calculates the closest rows between two matrices 
+#' (or data frames) considering pairwise differences between values in columns 
+#' of `x` and `table`. It returns the index of the closest row in `table` for 
+#' each row in `x`. 
 #'
-#' @param x `numeric`  matrix or data frame representing the query data. each 
-#' row will be compared to 'table'. Need to have the same numbers of rows and in 
-#' the same order as 'table'.
+#' @param x `numeric` matrix or data frame representing the query data. Each 
+#' row in `x` will be compared to every row in `table`. Both `x` and `table` are 
+#' expected to have the same number of columns, and the columns are expected to 
+#' be in the same order.
 #'
 #' @param table `numeric` matrix or data frame containing the reference data to 
-#' be matched with each row of 'x'. Need to have the same numbers of rows and in 
-#' the same order as 'x'.
+#' be matched with each row of `x`. Each row in `table` will be compared to 
+#' every row in `x`. Both `table` and `x` are expected to have the same number 
+#' of columns, and the columns are expected to be in the same order.
 #'
 #' @param ppm `numeric` representing a relative, value-specific 
 #' parts-per-million (PPM) tolerance that is added to tolerance (default is 0). 
 #'
-#' @param tolerance `numeric` accepted tolerance.
+#' @param tolerance `numeric` accepted tolerance. Defaults to `tolerance = Inf`, 
+#' thus for each row in x the closest row in table is reported, regardless of 
+#' the magnitude of the (absolute) difference.
 #'
-#' @return `numeric` vector of indices indicating the closest row of 'table' for
-#' each row of 'x'.
+#' @return `integer` vector of indices indicating the closest row of `table` for
+#' each row of `x`. If no suitable match is found for a row in `x` based on the 
+#' specified `tolerance` and `ppm`, the corresponding index is set to `NA`.
 #' 
 #' @details
-#' If, for a row of 'x', 2 rows of 'table' are closest only the index of first 
+#' If, for a row of `x`, two rows of `table` are closest only the index of first 
 #' row will be returned. 
 #' 
-#' For both the 'tolerance' and 'ppm' arguments, if their length is different to 
-#' the number of rows of 'x' and 'table', the input argument will be replicated 
-#' to match it. 
-#' 
+#' For both the `tolerance` and `ppm` arguments, if their length is different to 
+#' the number of columns of `x` and `table`, the input argument will be 
+#' replicated to match it. 
 #'
 #' @author Philippine Louail
 #' 
@@ -37,10 +43,17 @@
 #' @export 
 #'
 #' @examples
-#'x <- data.frame(a = 1:5, b = 3:7)
-#'table <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45))
-#'ppm <- 0.5
-#'tolerance <- 25
+#' x <- data.frame(a = 1:5, b = 3:7)
+#' table <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45))
+#'
+#' ## Get for each row of `x` the index of the row in `table` with the smallest 
+#' difference of values (per column)
+#' mclosest(x, table)
+#'
+#' ## If the absolute difference is larger than `tolerance`, return `NA`. Note
+#' ## that the tolerance value of `25` is used for difference for each pairwise 
+#' ## column in `x` and `table`.
+#' mclosest(x, table, tolerance = 25)
 #'
 
 mclosest <- function(x,
