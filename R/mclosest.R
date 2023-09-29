e62c0459e701fed21c4f0465f80927013c143671
Philippine Louail <philippine.louail@outlook.com>
2023-09-29 12:36:17 +0200

    last update

--- R/mclosest.R
+++ R/mclosest.R
@@ -3,61 +3,83 @@
 #' @description
 #'
 #' `mclosest` functions calculate the closest rows in a pairwise manner.
-#' It returns the index of the closest row in 'table' for each row in 'x'
+#' It returns the index of the closest row in 'table' for each row in 'x'. 
 #'
-#' @param x `numeric`  matrix or data frame representing the query data.
+#' @param x `numeric`  matrix or data frame representing the query data. each 
+#' row will be compared to 'table'. Need to have the same numbers of rows and in 
+#' the same order as 'table'.
 #'
-#' @param table `numeric` matrix or data frame containing the reference data.
+#' @param table `numeric` matrix or data frame containing the reference data to 
+#' be matched with each row of 'x'. Need to have the same numbers of rows and in 
+#' the same order as 'x'.
 #'
-#' @param ppm `numeric` value specifying the parts per million tolerance for
-#'             considering values as equal (default is 0).
+#' @param ppm `numeric` representing a relative, value-specific 
+#' parts-per-million (PPM) tolerance that is added to tolerance (default is 0). 
 #'
-#' @param tolerance `numeric` value specifiying  
+#' @param tolerance `numeric` accepted tolerance.
 #'
-#' @return `numeric` vector of indices indicating the closest row of 'table' for each row of 'x'
+#' @return `numeric` vector of indices indicating the closest row of 'table' for
+#' each row of 'x'.
+#' 
+#' @details
+#' If, for a row of 'x', 2 rows of 'table' are closest only the index of first 
+#' row will be returned. 
+#' 
+#' For both the 'tolerance' and 'ppm' arguments, if their length is different to 
+#' the number of rows of 'x' and 'table', the input argument will be replicated 
+#' to match it. 
+#' 
 #'
 #' @author Philippine Louail
+#' 
+#' @importFrom MsCoreUtils ppm
 #'
-#' @export
+#' @export 
 #'
 #' @examples
-x <- data.frame(a = 1:5, b = 3:7)
-table <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45))
-ppm <- 0.5
-tolerance <- 0.5
+#'x <- data.frame(a = 1:5, b = 3:7)
+#'table <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45))
+#'ppm <- 0.5
+#'tolerance <- 25
 #'
 
 mclosest <- function(x,
                      table,
                      ppm = 0,
                      tolerance = Inf) {
-  # sanity checks
+  ## sanity checks
   if (is.null(dim(x)))
     stop("'x' needs to be an array")
   if (is.null(dim(table)))
     stop("'table' needs to be an array")
   if (ncol(x) != ncol(table))
-    stop(" 'x' and 'table' need to have same number of columns")
+    stop("'x' and 'table' need to have same number of columns")
   if (!is.matrix(x))
     x <- as.matrix(x)
   if (!is.matrix(table))
     table <- as.matrix(table)
-  nr <- nrow(table)
   nc <- ncol(x)
+  nr <- nrow(table)
   if (length(ppm) != nc)
    ppm <- rep(ppm[1], nc)
   if (length(tolerance) != nc)
    tolerance <- rep(tolerance[1], nc)
 
-  # Initialize a vector to store closest row indices
-  closest_indices <- numeric(nr)
-  for (i in seq_len(nr)) {
-    abdiff <- abs(table - rep(x[i, ], each = nr))
+  ## Initialize a vector to store closest row indices
+  closest_indices <- rep(NA_integer_ , nrow(x))
+  for (i in seq_len(nrow(x))) {
+    abdiff <- abs(table - rep(x[i, ], each = nrt))
     ## Remove differences lower than tolerance
-    abdiff[abdiff < tolerance] <- NA
+    abdiff[abdiff > rep((tolerance + ppm(x[i,], ppm)), each = nrt)] <- NA
     ranked <- apply(abdiff, 2, rank, na.last="keep")
     rowProd <- apply(ranked, 1, prod)
-    closest_indices[i] <- which.min(rowProd)
+    res <- which.min(rowProd)
+    if (length(res)) 
+    closest_indices[i] <- res
   }
   closest_indices
 }
+
+
+
+    
--- tests/testthat/test_mclosest.R
+++ tests/testthat/test_mclosest.R
@@ -1,12 +1,25 @@
-test_that("mclosest works", {
-  ## load test data
-  x <- data.frame(a = 1:5, b = 3:7)
-  table <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45))
-  ppm <- 0.5
-  mclosest(x, table, ppm = 0.5)
+#' define some variable for test 
+x <- data.frame(a = 1:5, b = 3:7)
+table <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45))
+table_bigger <- data.frame(c = c(11, 23, 3, 5, 1), d = c(32:35, 45), c= (1:5))
+
+
+
+test_that("closest checks format", {
+    expect_error(mclosest(1:2, table), "'x' needs to be an array")
+    expect_error(mclosest(x, "test" ), "'table' needs to be an array")
+    expect_error(mclosest(x, table_bigger), "'x' and 'table' need to have same number of columns")
 })
 
+x <- x[1, drop = FALSE]
+test_that("one row works", {
+    expect_equal(mclosest(x1, table), 1)
+})
 
-#testing format:checking error, checking equal  
-# check for input one row (check_equal)
-# check for multiple row (check_equal)
+test_that("loop works", {
+   expect_equal(mclosest(x, table), c(1, 1, 1, 1, 1))
+})
+    
+testthat("tolerance test", {
+    expect_equal(mclosest(x, table, tolerance = 5), c(NA, NA, NA, NA, NA))
+})   
