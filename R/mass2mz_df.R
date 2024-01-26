69fa020fe42d9395e874229af35dad5a59aab90e
James Jiada Zhan <jzha832@emory.edu>
2024-01-25 20:58:39 -0500

    Update mass2mz_df.R

--- R/mass2mz_df.R
+++ R/mass2mz_df.R
@@ -7,10 +7,11 @@ mass2mz_df <- function(mass, adduct = "M+H") {
     # left join the mass_adduct dataframe with the adduct_definition dataframe
     mass_adduct_adduct_definition = left_join(mass_adduct, adduct_definition, by = c("adduct" = "name"))
     
-    mass_adduct_adduct_definition$adduct_mass = (mass_adduct_adduct_definition$mass + mass_adduct_adduct_definition$mass_add) / mass_adduct_adduct_definition$mass_multi
+    mass_adduct_adduct_definition$mz = (mass_adduct_adduct_definition$mass + mass_adduct_adduct_definition$mass_add) / mass_adduct_adduct_definition$mass_multi
 
     # select only the mass, adduct, and adduct_mass columns
-    mass_adduct_adduct_definition = mass_adduct_adduct_definition %>% select(mass, adduct, adduct_mass)
+    mass_adduct_adduct_definition = mass_adduct_adduct_definition %>% 
+        select(mass, adduct, mz)
 
     return(mass_adduct_adduct_definition)
 }
