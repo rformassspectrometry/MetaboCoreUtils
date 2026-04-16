b0233637afa00bae8accb52ab611a83bf5868b91
Gabriele Tomè <39188419+gabrieletome@users.noreply.github.com>
2026-04-16 14:38:48 +0200

    refactor: address Johannes comments.

--- R/translate.R
+++ R/translate.R
@@ -1,14 +1,16 @@
-#' @title Get supported software for column name translation
+#' @title Get supported software for name translation
 #'
 #' @description
 #'
-#' Get the names of the supported software defined in the mapping schema
+#' Get the names of the supported software defined in the default mapping schema
 #'
 #' @return A `character` vector with the names of the supported software.
 #'
 #' @export
 #'
 #' @author Gabriele Tomè
+#'
+#' @family name translation functions
 #'
 #' @importFrom utils read.delim
 #'
@@ -17,18 +19,24 @@
 #' softwareMapping()
 #'
 softwareMapping <- function() {
-    mapping_df <- read.delim(system.file("mappingSchema", "mapping_schema.tsv",
+    mapping_df <- read.delim(system.file("extdata", "mappingSchema",
+                                         "software_mapping_schema.tsv",
                                          package = "MetaboCoreUtils"),
                              header = TRUE, sep = "\t", check.names = FALSE)
     names(mapping_df)
 }
 
-#' @title Get mapping schema for column name translation
+#' @title Get mapping schema for name translation
 #'
 #' @description
 #'
 #' Get the mapping schema as a `data.frame` that defines the mapping between
-#' column names of different software.
+#' names of different software.
+#'
+#' @param path Optional `character(1)` with the path to a custom mapping schema
+#'     file in TSV format. If not provided, the default mapping schema defined
+#'     in the package will be used. The file must have a header row with
+#'     software names as names and the corresponding names as values.
 #'
 #' @return A `data.frame` with the mapping schema.
 #'
@@ -36,31 +44,47 @@ softwareMapping <- function() {
 #'
 #' @author Gabriele Tomè
 #'
+#' @family name translation functions
+#'
 #' @importFrom utils read.delim
 #'
 #' @examples
 #'
-#' mappingSchema()
+#' softwareMappingSchema()
 #'
-mappingSchema <- function() {
-    mapping_df <- read.delim(system.file("mappingSchema", "mapping_schema.tsv",
-                                         package = "MetaboCoreUtils"),
-                             header = TRUE, sep = "\t", check.names = FALSE,
-                             na.strings = "")
+softwareMappingSchema <- function(path = NULL) {
+    if (!is.null(path)) {
+        if (!file.exists(path)) {
+            stop("The provided file does not exist: '", path, "'.")
+        }
+        mapping_df <- read.delim(path, header = TRUE, sep = "\t",
+                                 check.names = FALSE, na.strings = "")
+    } else {
+        mapping_df <- read.delim(system.file("extdata", "mappingSchema",
+                                             "software_mapping_schema.tsv",
+                                             package = "MetaboCoreUtils"),
+                                header = TRUE, sep = "\t", check.names = FALSE,
+                                na.strings = "")
+    }
     mapping_df
 }
 
-#' @title Guess the source of column names
+#' @title Guess the source of names
 #'
 #' @description
 #'
-#' Guess the source of column names based on predefined mappings. The function
-#' counts how many elements in the input vector `x` match the column names
-#' defined in the mapping schema for each software and returns the software with
-#' the highest match count.
+#' Guess the source of names based on predefined mappings. The function counts
+#' how many elements in the input vector `x` match the names defined in the
+#' mapping schema for each software and returns the software with the highest
+#' match count.
 #'
-#' @param x A `character` vector of column names for which the source should be
-#' guessed.
+#' @param x A `character` vector of names for which the source should be
+#'     guessed.
+#'
+#' @param map Optional `data.frame` with a custom mapping schema. If not
+#'     provided, the default mapping schema defined in the package will be
+#'     used. The `data.frame` must contain the software names as column names
+#'     and the corresponding names as values.
 #'
 #' @return A `character(1)` with the name of the guessed source software.
 #'
@@ -68,18 +92,27 @@ mappingSchema <- function() {
 #'
 #' @author Gabriele Tomè
 #'
+#' @family name translation functions
+#'
 #' @examples
 #'
 #' ## MS-Dial names
 #' x <- c("Average Rt(min)", "Alignment ID", "Average Mz")
 #' guessSource(x)
 #'
-guessSource <- function(x = character()) {
+guessSource <- function(x = character(), map = NULL) {
     if (length(x) == 0) {
         stop("'x' must not be empty.")
     }
 
-    mapping_df <- mappingSchema()
+    if (!is.null(map)) {
+        if (!is.data.frame(map)) {
+            stop("'map' must be a data.frame.")
+        }
+        mapping_df <- map
+    } else {
+        mapping_df <- softwareMappingSchema()
+    }
 
     ## Count matches for each mapping
     match_counts <- vapply(mapping_df, function(mapping) {sum(x %in% mapping)},
@@ -93,14 +126,19 @@ guessSource <- function(x = character()) {
 #'
 #' @description
 #'
-#' Get a named `character` vector that defines the mapping between column names
-#' of two software based on the mapping schema. The names of the returned vector
-#' are the column names of the `from` software and the values are the
-#' corresponding column names of the `to` software.
+#' Get a named `character` vector that defines the mapping between names of two
+#' software based on the mapping schema. The names of the returned vector are
+#' the names of the `from` software and the values are the corresponding names
+#' of the `to` software.
 #'
 #' @param from `character(1)` with the name of the source software.
 #'
 #' @param to `character(1)` with the name of the target software.
+#'
+#' @param map Optional `data.frame` with a custom mapping schema. If not
+#'     provided, the default mapping schema defined in the package will be
+#'     used. The `data.frame` must contain the `from` and `to` parameter as
+#'     columns names.
 #'
 #' @return A named `character` vector with the mapping between the two software.
 #'
@@ -108,11 +146,13 @@ guessSource <- function(x = character()) {
 #'
 #' @author Gabriele Tomè
 #'
+#' @family name translation functions
+#'
 #' @examples
 #'
 #' nameMapping(from = "MS-Dial", to = "mzTab-M")
 #'
-nameMapping <- function(from = character(), to = character()) {
+nameMapping <- function(from = character(), to = character(), map = NULL) {
     if (length(from) == 0 || length(to) == 0) {
         stop("Both 'from' and 'to' must be specified.")
     }
@@ -121,42 +161,54 @@ nameMapping <- function(from = character(), to = character()) {
         stop("'from' and 'to' must be different.")
     }
 
-    if (!from %in% softwareMapping() || !to %in% softwareMapping()) {
-        stop("Both 'from' and 'to' must be valid software names. Use ",
-             "'softwareMapping()' to see available options.")
+    if (!is.null(map)) {
+        if (!is.data.frame(map)) {
+            stop("'map' must be a data.frame.")
+        }
+        if (!all(c(from, to) %in% names(map))) {
+            stop("'map' must contain columns named '", from, "' and '", to,
+                 "'.")
+        }
+        mapping_df <- map
+    } else {
+        if (!all(c(from, to) %in% softwareMapping())) {
+            stop("Both 'from' and 'to' must be valid software names. Use ",
+                 "'softwareMapping()' to see available options.")
+        }
+        mapping_df <- softwareMappingSchema()
     }
-
-    mapping_df <- mappingSchema()
 
     mapping <- setNames(mapping_df[[to]], mapping_df[[from]])
     mapping <- mapping[!(is.na(names(mapping)) & is.na(mapping))]
     mapping
 }
 
-#' @title Translate column names based on a provided mapping
+#' @title Translate names based on a provided mapping
 #'
 #' @description
 #'
-#' Map column names from one software to another. The function replaces elements
+#' Map names from one software to another. The function replaces elements
 #' in the input vector `x` with their corresponding values in the mapping if a
 #' match is found. If an element in `x` does not have a corresponding mapping,
 #' it is returned unchanged with a warning.
 #'
-#' @param x `character` vector of column names to be translated.
+#' @param x `character` vector of names to be translated.
 #'
 #' @param mapping A named `character` vector that defines the mapping for
-#'     translation. The names of the vector should be the original column names
-#'     and the values should be the translated column names.
+#'     translation. The names of the vector should be the original names
+#'     and the values should be the translated names.
 #'
-#' @return A `character` vector with the translated column names.
+#' @return A `character` vector with the translated names.
 #'
 #' @export
 #'
 #' @author Gabriele Tomè
 #'
+#' @family name translation functions
+#'
 #' @examples
 #'
-#' ## MS-D column names
+#' ## MS-Dial names
 #' x <- c("Average Rt(min)", "Alignment ID", "Average Mz")
 #' map_vec <- nameMapping(from = "MS-Dial", to = "mzTab-M")
 #' translate(x, mapping = map_vec)
--- tests/testthat/test_translate.R
+++ tests/testthat/test_translate.R
@@ -4,17 +4,46 @@ test_that("softwareMapping works", {
     expect_true(length(software) > 0)
 })
 
-test_that("mappingSchema works", {
-    mapping_df <- mappingSchema()
+test_that("softwareMappingSchema works", {
+    expect_error(softwareMappingSchema(path = "non_existent_file.tsv"),
+                 "The provided file does not exist")
+
+    ## Test custom schema loading
+    custom_schema <- data.frame(
+        "MS-Dial" = c("Average Rt(min)", "Alignment ID", "Average Mz"),
+        "custom" = c("custom_1", "custom_2", "custom_3"), check.names = FALSE
+    )
+    temp_file <- tempfile(fileext = ".tsv")
+    write.table(custom_schema, temp_file, sep = "\t",)
+    mapping_df <- softwareMappingSchema(path = temp_file)
+    expect_true(is.data.frame(mapping_df))
+    expect_equal(names(mapping_df), c("MS-Dial", "custom"))
+    expect_equal(nrow(mapping_df), 3)
+
+    ## Test default schema loading
+    mapping_df <- softwareMappingSchema()
     expect_true(is.data.frame(mapping_df))
 })
 
 test_that("guessSource works", {
+    ## Test default mapping schema
     x_ms_dial <- c("Average Rt(min)", "Alignment ID", "Average Mz")
     expect_equal(guessSource(x_ms_dial), "MS-Dial")
 
+    ## Test default mapping schema with mix input vector
     x_mix <- c("organism_name", "organism_lineage", "mzmed", "Average Rt(min)")
     expect_equal(guessSource(x_mix), "RforMassSpectrometry")
+
+    ## Test custom mapping schema
+    expect_error(guessSource(x = c("custom_1", "custom_2"), map = "not_df"),
+                 "'map' must be a data.frame")
+
+    custom_schema <- data.frame(
+        "MS-Dial" = c("Average Rt(min)", "Alignment ID", "Average Mz"),
+        "custom" = c("custom_1", "custom_2", "custom_3"), check.names = FALSE
+    )
+    x_custom <- c("custom_1", "custom_3")
+    expect_equal(guessSource(x_custom, map = custom_schema), "custom")
 })
 
 test_that("nameMapping works", {
@@ -27,9 +56,30 @@ test_that("nameMapping works", {
     expect_error(nameMapping(from = "MS-Dial", to = "Invalid"),
                  "must be valid software names")
 
+    ## Test default mapping schema
     map_vec <- nameMapping(from = "MS-Dial", to = "mzTab-M")
     expect_true(is.character(map_vec))
     expect_true(length(map_vec) > 0)
+
+    ## Test custom mapping schema
+    custom_schema <- data.frame(
+        "MS-Dial" = c("Average Rt(min)", "Alignment ID", "Average Mz"),
+        "custom" = c("custom_1", "custom_2", "custom_3"), check.names = FALSE
+    )
+    expect_error(nameMapping(from = "MS-Dial", to = "custom", map = "not_df"),
+                 "'map' must be a data.frame")
+    expect_error(nameMapping(from = "MS-Dial", to = "invalid",
+                             map = custom_schema),
+                 "must contain columns")
+    expect_error(nameMapping(from = "invalid", to = "custom",
+                             map = custom_schema),
+                 "must contain columns")
+
+    map_vec_custom <- nameMapping(from = "MS-Dial", to = "custom",
+                                  map = custom_schema)
+    expect_equal(map_vec_custom, c("Average Rt(min)" = "custom_1",
+                                   "Alignment ID" = "custom_2",
+                                   "Average Mz" = "custom_3"))
 })
 
 test_that("translate works", {
